1e7abddfc49b19993d853254cc15799195bd8c95
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-03-11 21:34:14 +0300

    (lab.2.2)

---
+++ lab2.2.R
@@ -0,0 +1,57 @@
+library(ggplot2)
+library(readxl)
+
+setwd("C:/Users/soch1van/Desktop/дз/R labs")
+data <- read.csv("data.csv", row.names = 1)
+data <- data[-(1)]
+#-------------------------------------------
+par(mfrow = c(1, 4))
+for (col in colnames(data)) {
+  hist(data[[col]], 
+       main = paste(col), 
+       xlab = "Оценка", 
+       ylab = "Частота", 
+       col = "lightblue", 
+       breaks = 10)
+}
+
+par(mar = c(12,4,4,2))
+boxplot(data, main = "boxplot для всех ресторанов", xlab = "Рестораны", ylab = "Значения", col = "lightblue",las = 2)
+par(mar = c(5,4,4,2))
+
+data_stats <- summary(data)
+print(data_stats)
+#----------------------------------------------
+data_sort <- data[order(data$Уни), ]
+#----------------------------------------------
+tori_selected <- subset(data, Tori.ramen > 7)
+print(tori_selected)
+print(dim(tori_selected))
+
+tori_selected_stats <- summary((tori_selected))
+print(tori_selected_stats)
+
+par(mar = c(12,4,4,2))
+boxplot(tori_selected, main = "boxplot для Tori >7", xlab = "Рестораны", ylab = "Значения", col = "lightblue",las = 2)
+par(mar = c(5,4,4,2))
+
+ggplot(tori_selected, aes(x = as.factor(Tori.ramen))) +
+  geom_bar(fill = "black", color = "black", position = "dodge") +
+  labs(title = "Рис.1. Гистограмма всех оценок Tori.ramen", x = "Оценка Tori.ramen", y = "Частота")
+#-----------------------------------------------
+data1 <- read_excel("data1.xlsx")
+data2 <- read_excel("data2.xlsx")
+
+data_main <- merge(data1, data2, by = "Фамилия")
+
+data3 <- read_excel("data3.xlsx")
+
+data_main <- rbind(data_main, data3)
+
+places <- names(data_main) %in% c("KFC", "Уни") 
+data_new <- data_main[!places]
+remove(places)
+
+data_new <- data_new[c(-1,-7)]
+
+data_subset <- subset(data_main, KFC > 0.7 | Уни < 0.4, select=c(Фамилия, KFC, Уни))
